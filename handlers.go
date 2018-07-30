d88027289d095ac069e7997b91dd2a80e54d0868
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-07-31 06:36:41 +0900

    modfy handler

--- handlers.go
+++ handlers.go
@@ -6,25 +6,43 @@ import (
 	"fmt"
 	"html"
 	"encoding/json"
+	"strconv"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-	fmt.Fprintf(w ,"hello ,%q", html.EscapeString(r.URL.Path))
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "hello ,%q", html.EscapeString(r.URL.Path))
 }
 
-func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
+func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(todos); err != nil{
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
 }
 
-func TodoShow(w http.ResponseWriter, t *http.Request, ps httprouter.Params){
-	fmt.Fprintf(w, "Todo show: %s", ps.ByName("todoId"))
+func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	idParam := ps.ByName("todoId")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	t := RepoFindTodo(id)
+	if t.ID == 0 && t.Name == "" {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.statusNoFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
 }
